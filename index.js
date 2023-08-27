678713609aec42ef6bc5ff793be7b1e654641406
Jamar Graves <jamargraves07@gmail.com>
2023-08-26 21:52:09 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+const newHeader = document.createElement("H1");
+newHeader.id = "victory"
+newHeader.textContent ="Jamar is the champion"
+
+
+
+document.body.append(newHeader);
+
